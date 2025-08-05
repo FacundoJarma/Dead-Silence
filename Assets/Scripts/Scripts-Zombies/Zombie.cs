3f2476068dadb328c0e70f3af7a9cafb6d8dae4d
santi901 <49379041@est.ort.edu.ar>
2025-08-05 14:31:08 -0300

    Zombies path finding

---
+++ Assets/Scripts/Scripts-Zombies/Zombie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour
+{
+    public Transform destino;          // El punto al que se moverá el zombie
+    private NavMeshAgent agente;       // El componente NavMeshAgent
+
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();  // Obtener el agente
+        if (destino != null)
+        {
+            agente.SetDestination(destino.position);  // Mandarlo al destino
+        }
+        else
+        {
+            Debug.LogWarning("No se asignó un destino al zombie.");
+        }
+    }
+}
