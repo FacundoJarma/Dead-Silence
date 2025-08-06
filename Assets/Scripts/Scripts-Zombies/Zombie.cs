f55d7ab7c71ed9d709b2df036536743a339f3485
santi901 <49379041@est.ort.edu.ar>
2025-08-06 10:05:07 -0300

    Terminado path finding

--- Assets/Scripts/Scripts-Zombies/Zombie.cs
+++ Assets/Scripts/Scripts-Zombies/Zombie.cs
@@ -5,19 +5,51 @@ using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
-    public Transform destino;          // El punto al que se moverá el zombie
     private NavMeshAgent agente;       // El componente NavMeshAgent
+
+    private bool persiguiendoJugador = false;
+    private float tiempoTranscurrido = 0f;
+    float tiempoAntesDePerseguir = 1f; // Tiempo antes de seguir al jugador
+
+    public GameObject jugador;
 
     void Start()
     {
         agente = GetComponent<NavMeshAgent>();  // Obtener el agente
-        if (destino != null)
+        if (jugador != null)
         {
-            agente.SetDestination(destino.position);  // Mandarlo al destino
+            agente.SetDestination(jugador.transform.position);  // Mandarlo al destino
         }
         else
         {
             Debug.LogWarning("No se asignó un destino al zombie.");
         }
     }
+    void Update()
+    {
+        tiempoTranscurrido += Time.deltaTime;
+
+        // Después de cierto tiempo, cambiar a persecución
+        if (!persiguiendoJugador && tiempoTranscurrido >= tiempoAntesDePerseguir)
+        {
+            persiguiendoJugador = true;
+        }
+
+        // Si está en modo persecución, seguir al jugador
+        if (persiguiendoJugador && jugador != null)
+        {
+            agente.SetDestination(jugador.transform.position);
+        }
+    }
 }
+
+    
+   
+   
+
+    
+
+   
+    
+
+
