6e8b5c96d7bae39ae134f4e81612c249c383033d
FacundoJarma <49127963@est.ort.edu.ar>
2025-08-06 10:04:27 -0300

    Item prefab y player con interactable Indicator

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     public List<Item> inventory = new List<Item>();
+    public int maxSize;
 
     public void AddItem(Item i)
     {
         //TODO: Chequeos previos
+        if (inventory.Count >= maxSize)
+        {
+            Debug.Log("Inventario lleno. No se puede añadir el item.");
+            return;
+        }
 
         inventory.Add(i);
         Debug.Log("Item añadido correctamente!");
