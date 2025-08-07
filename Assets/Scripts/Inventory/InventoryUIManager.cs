4895e9866479d04160c1083fe218163cbfbe0752
FacundoJarma <49127963@est.ort.edu.ar>
2025-08-07 11:57:29 -0300

    UI basica para el inventario

--- Assets/Scripts/Inventory/InventoryUIManager.cs
+++ Assets/Scripts/Inventory/InventoryUIManager.cs
@@ -7,13 +7,31 @@ public class InventoryUIManager : MonoBehaviour
     public GameObject inventoryUI;
 
     public InventoryManager inventoryManager;
+    public GameObject InventoySlot;
+
+    bool isOpen = false;
 
     void Start()
     {
         inventoryManager.onInventoryChanged += UpdateUI;
     }
 
-
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (isOpen)
+            {
+                Close();
+                isOpen = false;
+            }
+            else
+            {
+                Open();
+                isOpen = true;
+            }
+        }
+    }
     public void Open()
     {
         inventoryUI.SetActive(true);
@@ -26,6 +44,14 @@ public class InventoryUIManager : MonoBehaviour
 
     public void UpdateUI()
     {
+        foreach (Transform child in inventoryUI.transform)
+        {
+            Destroy(child.gameObject);
+        }
 
+        for (int i = 0; i < inventoryManager.inventory.Count; i++)
+        {
+            GameObject slot = Instantiate(InventoySlot, inventoryUI.transform);
+        }
     }
 }
