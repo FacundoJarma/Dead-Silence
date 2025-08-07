96754c60f9f49079df1505a98665704e959b3a79
FacundoJarma <49127963@est.ort.edu.ar>
2025-08-07 10:40:14 -0300

    Empezar la UI basica del inventario

---
+++ Assets/Scripts/Inventory/InventoryUIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUIManager : MonoBehaviour
+{
+    public GameObject inventoryUI;
+
+    public InventoryManager inventoryManager;
+
+    void Start()
+    {
+        inventoryManager.onInventoryChanged += UpdateUI;
+    }
+
+
+    public void Open()
+    {
+        inventoryUI.SetActive(true);
+    }
+
+    public void Close()
+    {
+        inventoryUI.SetActive(false);
+    }
+
+    public void UpdateUI()
+    {
+
+    }
+}
