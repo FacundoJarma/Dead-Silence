af48892973882a978a8f28a6203cd3de6c386f0c
FacundoJarma <49127963@est.ort.edu.ar>
2025-08-08 09:59:35 -0300

    El inventario ahora renderiza los iconos

--- Assets/Scripts/Inventory/InventoryUIManager.cs
+++ Assets/Scripts/Inventory/InventoryUIManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class InventoryUIManager : MonoBehaviour
 {
     public GameObject inventoryUI;
@@ -52,6 +52,17 @@ public class InventoryUIManager : MonoBehaviour
         for (int i = 0; i < inventoryManager.inventory.Count; i++)
         {
             GameObject slot = Instantiate(InventoySlot, inventoryUI.transform);
+            Item item = inventoryManager.inventory[i];
+
+            Transform iconTransform = slot.transform.Find("InventorySlot Image"); 
+            if (iconTransform != null)
+            {
+                Image sr = iconTransform.GetComponent<Image>();
+                if (sr != null)
+                {
+                    sr.sprite = item.icon;
+                }
+            }
         }
     }
 }
