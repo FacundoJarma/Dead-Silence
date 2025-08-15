a87662d6f2fe2be2876a0384c21269064b2f22c6
FacundoJarma <49127963@est.ort.edu.ar>
2025-08-14 21:59:26 -0300

    Añadir objetos movibles

---
+++ Assets/Scripts/MovableObject.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class MovableObject : MonoBehaviour, IInteractable
+{
+    public bool isHolding = false;
+    public float moveForce = 5f;
+    public float maxSpeed = 3f; // límite de velocidad
+    private Rigidbody rb;
+
+
+    bool canHold;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Interact()
+    {
+        if (canHold)
+        {
+            isHolding = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isHolding)
+        {
+            // Dirección horizontal hacia donde mira la cámara
+            Vector3 direccion = Camera.main.transform.forward;
+            direccion.y = 0;
+
+            // Si no ha alcanzado la velocidad máxima, aplicamos fuerza
+            if (rb.velocity.magnitude < maxSpeed)
+            {
+                rb.AddForce(direccion.normalized * moveForce, ForceMode.Acceleration);
+            }
+        }
+        else
+        {      
+            rb.velocity *= 0.5f;      
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isHolding = false;
+            canHold = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canHold = true;
+        }
+    }
+}
