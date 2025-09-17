57ca8673c2fe44623f38b96f67192c08b4dbc484
FacundoJarma <49127963@est.ort.edu.ar>
2025-09-17 10:01:07 -0300

    arreglar error

--- Assets/Scripts/Zombies/Behaviour Tree/VolverAPatrullar.cs
+++ Assets/Scripts/Zombies/Behaviour Tree/VolverAPatrullar.cs
@@ -6,7 +6,7 @@ public class VolverAPatrullar : NodoBT
 {
      private Zombie zombie;
 
-    public void VolverAPatrullarNodo(Zombie z)
+    public VolverAPatrullar(Zombie z)
     {
         zombie = z;
     }
